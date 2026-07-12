09f5b2d3593438b62e9c44d9ae55ac436c1169d0
Sangam00007 <sangamtimalsina55@gmail.com>
2026-07-12 19:11:31 +0545

    Create Fine model, migration, and controller

---
+++ app/Http/Controllers/FineController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FineController extends Controller
+{
+    //
+}
---
+++ app/Models/Fine.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fine extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'borrowing_id',
+        'amount',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function borrowing()
+    {
+        return $this->belongsTo(Borrowing::class);
+    }
+}
