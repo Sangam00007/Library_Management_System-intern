48271d36de97ca28c487778faa0d0ff7cb82bde9
Sangam00007 <sangamtimalsina55@gmail.com>
2026-07-12 19:10:09 +0545

    Create Borrowing model, migration, and controller

---
+++ app/Models/Borrowing.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Borrowing extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'borrow_date',
+        'due_date',
+        'return_date',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'borrow_date' => 'date',
+            'due_date' => 'date',
+            'return_date' => 'date',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
