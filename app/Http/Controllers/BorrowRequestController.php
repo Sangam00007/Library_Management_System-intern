9933fd88a3a5f331543407fcfcc7fe6dc01c0b41
Sangam00007 <sangamtimalsina55@gmail.com>
2026-07-12 19:09:27 +0545

    Create BorrowRequest model, migration, and controller

---
+++ app/Http/Controllers/BorrowRequestController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BorrowRequestController extends Controller
+{
+    //
+}
---
+++ app/Models/BorrowRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BorrowRequest extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
